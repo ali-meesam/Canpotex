c90799129f25d0a15d9a1b48d785c4301b506aa3
Meesam Ali <meesam.ali@usask.ca>
2022-08-19 09:58:11 -0400

    More datas added to the sample

--- datapipeline.py
+++ datapipeline.py
@@ -4,7 +4,6 @@ import pandas as pd
 class DataFeed:
     def __init__(self) -> None:
         self.src = "DataFeeds"
-        
 
     @property
     def get_eurusd(self):
@@ -17,3 +16,48 @@ class DataFeed:
         file_name = 'FoodPriceIndex_FAO.xlsx'
         src_file = os.path.join(self.src,file_name)
         return pd.read_excel(src_file,index_col=0, parse_dates=True)
+
+    @property
+    def get_g20_cpi(self):
+        file_name = 'G20CPI.xlsx'
+        src_file = os.path.join(self.src,file_name)
+        return pd.read_excel(src_file,index_col=0, parse_dates=True)
+
+    @property
+    def get_gdp(self):
+        file_name = 'GDP_Steo.xlsx'
+        src_file = os.path.join(self.src,file_name)
+        df = pd.read_excel(src_file)
+        df['Date'] = pd.to_datetime(df.Month+"-"+df.Year.apply(lambda x:str(x)))
+        df = df[['Date','GDPQXUS']]
+        df.set_index('Date',inplace=True)
+        return df
+
+    @property
+    def get_historical_netback(self):
+        file_name = 'Historical_Actual_Netback.xlsx'
+        src_file = os.path.join(self.src,file_name)
+        df = pd.read_excel(src_file,index_col=0,parse_dates=True)
+        return df
+
+    @property
+    def get_natural_gas(self):
+        file_name = 'Natural_Gas_Steo.xlsx'
+        src_file = os.path.join(self.src,file_name)
+        df = pd.read_excel(src_file)
+        df['Date'] = pd.to_datetime(df.Month+"-"+df.Year.apply(lambda x:str(x)))
+        df = df[['Date','NGHHUUS']]
+        df.set_index('Date',inplace=True)
+        return df
+
+    @property
+    def get_total_fertilizer_production(self):
+        file_name = 'TotalFertilizerProduction.xlsx'
+        src_file = os.path.join(self.src,file_name)
+        return pd.read_excel(src_file,index_col=0, parse_dates=True)
+
+    @property
+    def get_POT_price(self):
+        file_name = 'POT_Stock_Price.csv'
+        src_file = os.path.join(self.src,file_name)
+        return pd.read_csv(src_file,index_col=0, parse_dates=True)
